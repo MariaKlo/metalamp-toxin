189fa4787fdc3386e160512f1bc47be6577b4562
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-09-15 20:30:47 +0300

    Update buttons, add new files
    Here I updated checkbox and radio buttons a little, added new files for the rest of ui-kit, started creating a markup for cards section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import './sass/colors.sass';
 import './sass/fonts.sass';
 import './sass/formElements.sass';
+import './sass/cards.sass';
+import './sass/headersFooters.sass'
 import logo from './img/logo.png';
