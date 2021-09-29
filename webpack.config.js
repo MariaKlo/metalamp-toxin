fdd640cbf28218f159fdc7020950b7be1783bf7a
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-09-29 22:37:46 +0300

    Update webpack config
    Added svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       }, 
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svgo-loader',
+          }
+        ]
+      },
     ]
   }
 };
