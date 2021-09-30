9740ea79bb0091ff83ca34195ea31ec50ae8618f
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-09-30 18:21:17 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import './sass/colors.sass';
+import './sass/colors-type.sass';
 import './sass/fonts.sass';
-import './sass/formElements.sass';
+import './sass/form-elements.sass';
 import './sass/cards.sass';
-import './sass/headersFooters.sass'
-import logo from './img/logo.png';
+import './sass/headers-footers.sass';
