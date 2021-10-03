dfe81e4a3cedf4412e1a154ef8f752b340b5375a
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-10-03 18:56:01 +0300

    Add slider
    I added slider for cards page. Still working not properly

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
+// import _ from "underscore";
+// import $ from "expose-loader?exposes=$,jQuery!jquery";
+// import _, { map } from 'underscore';
+import $ from 'jquery';
+
+// require("inputmask/dist/inputmask/jquery.inputmask.js");
+
 import './sass/colors-type.sass';
 import './sass/fonts.sass';
 import './sass/form-elements.sass';
 import './sass/cards.sass';
 import './sass/headers-footers.sass';
+import './sass/swiper.sass';
+
+global.jQuery = global.$ = $;
