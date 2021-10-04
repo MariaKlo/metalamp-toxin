f4aeac2741461617b374e9c9cc36492f15446a0c
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-10-04 21:02:18 +0300

    Failed attempt to add plugins
    Failed to add plugins air-datepicker and item-quantity-dropdown

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // import _ from "underscore";
 // import $ from "expose-loader?exposes=$,jQuery!jquery";
 // import _, { map } from 'underscore';
+// import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
+// require("inputmask/dist/inputmask/jquery.inputmask.js");
 import $ from 'jquery';
 
-// require("inputmask/dist/inputmask/jquery.inputmask.js");
+// import 'item-quantity-dropdown';
+// import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
 
 import './sass/colors-type.sass';
 import './sass/fonts.sass';
@@ -13,3 +17,7 @@ import './sass/headers-footers.sass';
 import './sass/swiper.sass';
 
 global.jQuery = global.$ = $;
+
+// $(document).ready(() => {
+//     $('.iqdropdown').iqDropdown( [options] );
+// })
