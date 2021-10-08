3938cc8a8a0a6e027b6f5a7d36ffffdae2876394
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-10-08 19:21:26 +0300

    Update datepicker

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// require("inputmask/dist/inputmask/jquery.inputmask.js");
+// import 'item-quantity-dropdown';
+// import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
+
+// import * as noUiSlider from 'nouislider';
+// import 'nouislider/dist/nouislider.css';
+
+import $ from 'jquery';
+import './sass/main.sass';
+import './sass/fonts.sass';
+
+import './js/plugins/datepicker';
+
+global.jQuery = global.$ = $;
