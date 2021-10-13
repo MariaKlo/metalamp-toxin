67de1b552713093f9ff61783e111b8f7d2e73929
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-10-13 19:33:22 +0300

    fix minor bugs, styling iqdropdown plugin

--- src/js/plugins/item-quantity-dropdown.js
+++ src/js/plugins/item-quantity-dropdown.js
@@ -1,6 +1,13 @@
-import './item-quantity-dropdown.js';
+import './item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
-$(document).ready(function() {
-    $('.iqdropdown').iqDropdown({options});
+$(document).ready(() => {
+    $('.iqdropdown').iqDropdown({
+      setSelectionText: (itemCount, totalItems) => {
+        // if (itemCount === 1) return `${totalItems} гость`
+        // if (itemCount >= 2 && itemCount <= 4) return `${totalItems} гостя`
+        // if (itemCount >= 5 && itemCount <= 20) return `${totalItems} гостей`
+        return `Сколько гостей`
+    },
+  });
 });
