4b8f7d55283e5df389850365f5cd2e48825c485f
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-10-26 21:10:06 +0300

    Update package.json, working on iqdropdown

--- src/js/plugins/item-quantity-dropdown.js
+++ src/js/plugins/item-quantity-dropdown.js
@@ -3,58 +3,71 @@ import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
 $(document).ready(() => {
     $('.iqdropdown').iqDropdown({
-    onChange: (id, count, totalItems) => {
-      $( "#iqdropdown-btn-clear" ).click(function() {
-        // const itemCount = {};
-        // const $counter = $(`<span>${itemCount[id]}</span>`).addClass("counter");
-        // itemCount[id] = 0;
-        // totalItems = 0;
-        // $counter.html(itemCount[id]);
-        $(".counter").text("0");
-        $( ".iqdropdown-selection" ).text( "Сколько гостей" );
-      });
-      $("#iqdropdown-btn-confirm").click(function() {
-        $(".iqdropdown").removeClass(".menu-open");
-      });
-    },
-    setSelectionText: (itemCount, totalItems) => {
-      itemCount = 0;
-      if ($(".counter").val(itemCount)) {
-        $("button-decrement").css("opacity", "0.5");
-      }
-      if (totalItems === 0) {
-        $("#iqdropdown-btn-clear").hide();
-      } else {
-        $("#iqdropdown-btn-clear").show();
-      }
+    setSelectionText: (id, itemCount, totalItems) => {
       // set up the grammar of the word "гость"
       const textPlural = `гостей`;
       const selectionText = `гость`;
       const placeholderText = `Сколько гостей`;
       const secondSelectionText = `гостя`;
       if (totalItems === 0) return placeholderText;
-      let arrayOfTotalItems = totalItems.toString(10).split("").map(Number);
-      if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
-         arrayOfTotalItems[arrayOfTotalItems.length-1] === 1) {
-        return `${totalItems} ${textPlural}`;
-      } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
-         arrayOfTotalItems[arrayOfTotalItems.length-1] === 2) {
-        return `${totalItems} ${textPlural}`;
-      } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
-         arrayOfTotalItems[arrayOfTotalItems.length-1] === 3) {
-        return `${totalItems} ${textPlural}`;
-      } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
-         arrayOfTotalItems[arrayOfTotalItems.length-1] === 4) {
-        return `${totalItems} ${textPlural}`;
-      } else if (arrayOfTotalItems[arrayOfTotalItems.length-1] === 1) {
-        return `${totalItems} ${selectionText}`;
-      } else if (arrayOfTotalItems[arrayOfTotalItems.length-1] === 2 ||
-         arrayOfTotalItems[arrayOfTotalItems.length-1] === 2 ||
-          arrayOfTotalItems[arrayOfTotalItems.length-1] === 3 ||
-           arrayOfTotalItems[arrayOfTotalItems.length-1] === 4) {
-        return `${totalItems} ${secondSelectionText}`;
-      } 
-      return `${totalItems} ${textPlural}`;
+      // let arrayOfTotalItems = Array(totalItems).fill('');
+      // let arrayOfTotalItems = [];
+      // for (let i = 0; i <= totalItems; i++) {
+      //   arrayOfTotalItems.push(i);
+      // }
+      // let arrayOfTotalItems = totalItems.toString(10).split("").map(Number);
+      // if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
+      //    arrayOfTotalItems[arrayOfTotalItems.length-1] === 1) {
+      //   return `${totalItems} ${textPlural}`;
+      // } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
+      //    arrayOfTotalItems[arrayOfTotalItems.length-1] === 2) {
+      //   return `${totalItems} ${textPlural}`;
+      // } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
+      //    arrayOfTotalItems[arrayOfTotalItems.length-1] === 3) {
+      //   return `${totalItems} ${textPlural}`;
+      // } else if (arrayOfTotalItems[arrayOfTotalItems.length-2] === 1 &&
+      //    arrayOfTotalItems[arrayOfTotalItems.length-1] === 4) {
+      //   return `${totalItems} ${textPlural}`;
+      // } else if (arrayOfTotalItems[arrayOfTotalItems.length-1] === 1) {
+      //   return `${totalItems} ${selectionText}`;
+      // } else if (arrayOfTotalItems[arrayOfTotalItems.length-1] === 2 ||
+      //    arrayOfTotalItems[arrayOfTotalItems.length-1] === 2 ||
+      //     arrayOfTotalItems[arrayOfTotalItems.length-1] === 3 ||
+      //      arrayOfTotalItems[arrayOfTotalItems.length-1] === 4) {
+      //   return `${totalItems} ${secondSelectionText}`;
+      // } 
+      // return `${totalItems} ${textPlural}`;
+    },
+     onChange: (id, count, totalItems) => {
+      // $( "#iqdropdown-btn-clear" ).click(function() {
+        // let counter = $(".iqdropdown-menu-option").data("mincount");
+        // let itemId = $(".iqdropdown-menu-option").data("id");
+        // let newItemId = itemId.children(".button-decrement").addClass("first-decrement-button");
+        if (count === 0 && $(".iqdropdown-menu-option").attr("data-id", "item1")) {
+          $(".button-decrement").css({"opacity": "0.5"});
+        } 
+        else if (count === 0 && $(".iqdropdown-menu-option").attr("data-id", "item2")) {
+          $(".button-decrement").css({"opacity": "0.5"});
+        } else if (count === 0 && $(".iqdropdown-menu-option").attr("data-id", "item3")) {
+          $(".button-decrement").css({"opacity": "0.5"});
+        } 
+        else {
+          $(".button-decrement").css({"opacity": "1"});
+        }
+        // const itemCount = {};
+        // if (count == 0) {
+        //   $(".button-decrement").css("border-color", "rgba(31, 32, 65, 0.25)");
+        // }
+        // const $counter = $(`<span>${itemCount[id]}</span>`).addClass("counter");
+        // itemCount[id] = 0;
+        // totalItems = 0;
+        // $counter.html(itemCount[id]);
+        // $(".counter").text("0");
+        // $( ".iqdropdown-selection" ).text( "Сколько гостей" );
+      // });
+      $("#iqdropdown-btn-confirm").click(function() {
+        $(".iqdropdown").removeClass(".menu-open");
+      });
     },
   });
 });
