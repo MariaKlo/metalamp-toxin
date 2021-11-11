adc87cf96db1f637e7354133e8b254dbf46da81c
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-11-11 20:04:30 +0300

    Added first expandable checkbox list

--- src/main.js
+++ src/main.js
@@ -7,5 +7,6 @@ import './js/plugins/iqdropdown/item-quantity-dropdown.js';
 import './js/plugins/pagination/paginationjs.js';
 import './js/plugins/range-slider/slider.js';
 import './js/buttons/like-button.js';
+import './js/buttons/expandableCheckboxList.js';
 
 global.jQuery = global.$ = $;
