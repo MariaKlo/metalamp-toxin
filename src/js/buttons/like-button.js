7f69b5cc62ff7c47b7e4008bca421725d9ff914b
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-11-11 19:03:40 +0300

    Create new like buttons

--- src/js/buttons/like-button.js
+++ src/js/buttons/like-button.js
@@ -1,6 +1,24 @@
-let h1 = document.querySelector('.heartOne');
+let firstButton = document.querySelector('.like-buttons__border');
+let secondButton = document.querySelector('.like-buttons__border_second-like-button');
 let _svg1 = document.querySelector('svg.one');
+let _svg2 = document.querySelector('svg.two');
+let firstCounter = document.querySelector('.like-buttons__counter');
+let secondCounter = document.querySelector('.like-buttons__counter_second-like-button');
+let countLikesOfFirstButton = 0;
+let countLikesOfSecondButton = 0;
 
-h1.addEventListener('click',e=>{
-    _svg1.classList.toggle('on');
+firstButton.addEventListener('click', e=>{
+    _svg1.classList.add('on');
+    firstButton.classList.add('on');
+    firstCounter.classList.add('on');
+    countLikesOfFirstButton++;
+    firstCounter.innerHTML = countLikesOfFirstButton;
+}, false);
+
+secondButton.addEventListener('click', e=>{
+    _svg2.classList.add('on');
+    secondButton.classList.add('on');
+    secondCounter.classList.add('on');
+    countLikesOfSecondButton++;
+    secondCounter.innerHTML = countLikesOfSecondButton;
 }, false);
