45cfcf3d60005fe38c3d9a8eec4655ac6504c43f
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-11-12 19:31:22 +0300

    Trying fix bugs on swiper

--- src/js/plugins/swiper/swiper.js
+++ src/js/plugins/swiper/swiper.js
@@ -1,16 +1,26 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // core version + navigation, pagination modules:
+//   import Swiper, { Navigation, Pagination } from 'swiper';
+//   // import Swiper and modules styles
+//   import 'swiper/css';
+//   import 'swiper/css/navigation';
+//   import 'swiper/css/pagination';
+
+//   // configure Swiper to use modules
+// Swiper.use([Navigation, Pagination]);
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+// });
