f8f2c819bec2263544c8da4eba9b29c6ab6ba231
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-11-16 18:10:51 +0300

    Fixed swiper

--- src/js/plugins/swiper/swiper.js
+++ src/js/plugins/swiper/swiper.js
@@ -1,26 +1,26 @@
-  // core version + navigation, pagination modules:
-//   import Swiper, { Navigation, Pagination } from 'swiper';
-//   // import Swiper and modules styles
-//   import 'swiper/css';
-//   import 'swiper/css/navigation';
-//   import 'swiper/css/pagination';
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-//   // configure Swiper to use modules
-// Swiper.use([Navigation, Pagination]);
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
 
-// const swiper = new Swiper('.swiper', {
-//     // Optional parameters
-//     direction: 'horizontal',
-//     loop: true,
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
   
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-// });
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './js/plugins/datepicker/datepicker.js';
 import './js/plugins/iqdropdown/item-quantity-dropdown.js';
 import './js/plugins/pagination/paginationjs.js';
 import './js/plugins/range-slider/slider.js';
+import './js/plugins/swiper/swiper.js';
 import './js/buttons/like-button.js';
 import './js/buttons/expandableCheckboxList.js';
 
