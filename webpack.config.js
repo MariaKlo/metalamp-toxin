bee3753360e612da256852e5d4e7b48b8e8d30e6
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-11-29 19:17:34 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,127 @@
+const path = require('path');
+var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/main.js'],
+  devtool: 'source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    modules: ['node_modules'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/pages/index.pug'), // src/pages/index.pug
+    filename: "index.html",
+    minify: {
+      collapseWhitespace: isProd,
+    }
+  }),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: 'jquery',
+          entry: 'dist/jquery.min.js',
+          global: 'jQuery',
+        },
+      ],
+    }),
+    new webpack.ProvidePlugin( {
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+    new FaviconsWebpackPlugin({
+      logo: '/metalamp/TOXIN/src/img/Group.png',
+      cache: true,
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [ "style-loader", "css-loader", "sass-loader", ],
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        }
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }, 
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svgo-loader',
+          }
+        ]
+      },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+      {
+        test: require.resolve("underscore"),
+        loader: "expose-loader",
+        options: {
+          exposes: [
+            "_.map|map",
+            {
+              globalName: "_.reduce",
+              moduleLocalName: "reduce",
+            },
+            {
+              globalName: ["_", "filter"],
+              moduleLocalName: "filter",
+            },
+          ],
+        },
+      },
+    ]
+  }
+};
