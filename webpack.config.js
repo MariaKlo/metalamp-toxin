37a57b8b79a33f19b8e5f412fbd826691212b0fa
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-12-06 21:25:12 +0300

    Created landing page (multiple pages website)

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,61 @@ var webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const fs = require('fs');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
 
 const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+// const PATHS = {
+//   // src must be src
+//   src: path.join(__dirname, '../website/src'),
+//   // dist to public
+//   dist: path.join(__dirname, '../dist'),
+//   // assets to static
+//   assets: 'assets/'
+// }
+// const PAGES_DIR = `${PATHS.src}/pages/`
+// const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
 
 module.exports = {
   mode: 'development',
-  entry: ['./src/main.js'],
+  entry: {
+    main: './src/main.js',
+  },
   devtool: 'source-map',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   resolve: {
     modules: ['node_modules'],
   },
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+  },
   plugins: [
+    new MiniCssExtractPlugin(),
+    // ...PAGES.map(page => new HtmlWebpackPlugin({
+    //   template: `${PAGES_DIR}/${page}`,
+    //   filename: `./${page.replace(/\.pug/,'.html')}`
+    // })),
     new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/pages/index.pug'), // src/pages/index.pug
-    filename: "index.html",
-    minify: {
-      collapseWhitespace: isProd,
-    }
+    template: 'src/pages/landing-page/landing-page.pug',
+    filename: "./index.html",
   }),
-    new HtmlWebpackPlugin({
-    template: path.resolve(__dirname, 'src/pages/sign-in/sign-in.pug'),
-    filename: "sign-in.html",
-    minify: {
-      collapseWhitespace: isProd,
-    }
+  new HtmlWebpackPlugin({
+    template: 'src/pages/sign-in/sign-in.pug',
+    filename: "./sign-in.html",
+  }),
+  new HtmlWebpackPlugin({
+    template: 'src/pages/registration/registration.pug',
+    filename: "./registration.html",
   }),
     new HtmlWebpackExternalsPlugin({
       externals: [
@@ -110,23 +134,6 @@ module.exports = {
         loader: "expose-loader",
         options: {
           exposes: ["$", "jQuery"],
-        },
-      },
-      {
-        test: require.resolve("underscore"),
-        loader: "expose-loader",
-        options: {
-          exposes: [
-            "_.map|map",
-            {
-              globalName: "_.reduce",
-              moduleLocalName: "reduce",
-            },
-            {
-              globalName: ["_", "filter"],
-              moduleLocalName: "filter",
-            },
-          ],
         },
       },
     ]
