57e0d2dc7d9393b944d5e1b8337d2c49f291a9f7
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2021-12-08 21:54:28 +0300

    Add images, continue creating filter page

--- src/main.js
+++ src/main.js
@@ -6,5 +6,7 @@ import './pages/landing-page/header/header-main/header-cards/plugins/datepicker/
 import './pages/landing-page/header/header-main/header-cards/plugins/item-quantity-dropdown/item-quantity-dropdown.js';
 import './pages/filter/filter-main/filter-options/filter-buttons/expandable-checkbox-list/expandable-checkbox-list.js';
 import './pages/filter/filter-main/filter-options/filter-plugins/range-slider/range-slider.js';
+import './pages/filter/filter-main/filter-result/pagination/paginationjs.js';
+import './pages/filter/filter-main/filter-result/filter-card/swiper/swiper.js';
 
 global.jQuery = global.$ = $;
