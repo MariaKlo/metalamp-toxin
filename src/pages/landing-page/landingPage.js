628a4ec9235373d3e87ef15562d25e116e87de91
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-01-18 22:02:34 +0300

    Responsive for landing page is ready

--- src/pages/landing-page/landingPage.js
+++ src/pages/landing-page/landingPage.js
@@ -3,3 +3,4 @@ global.jQuery = global.$ = $;
 
 import './header/header-main/header-cards/plugins/datepicker/datepicker.js';
 import './header/header-main/header-cards/plugins/item-quantity-dropdown/item-quantity-dropdown.js';
+import './header/header-menu/hamburger/hamburger.js';
