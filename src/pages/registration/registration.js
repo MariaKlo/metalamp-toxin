a638bab9d03820f4913b948e30809049c3aa54f2
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-01-19 21:26:33 +0300

    Responsive for sign-up page is ready

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -2,3 +2,4 @@ import $ from 'jquery';
 global.jQuery = global.$ = $;
 
 import '../landing-page/header/header-main/header-cards/plugins/datepicker/datepicker.js';
+import '../landing-page/header/header-menu/hamburger/hamburger.js';
