b63368a25deb3f30f189423e6ec71b6cbc274083
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-07-08 19:16:26 +0300

    Refactoring form-elements (in process)

--- src/components/buttons/like-button/like-button.js
+++ src/components/buttons/like-button/like-button.js
@@ -1,34 +1,40 @@
-let firstButton = document.querySelector('.like-buttons__border');
-let secondButton = document.querySelector('.like-buttons__border_second-like-button');
-let thirdButton = document.querySelector('.like-buttons__border_third-like-button');
-let _svg1 = document.querySelector('svg.one');
-let _svg2 = document.querySelector('svg.two');
-let _svg3 = document.querySelector('svg.three');
-let firstCounter = document.querySelector('.like-buttons__counter');
-let secondCounter = document.querySelector('.like-buttons__counter_second-like-button');
-let thirdCounter = document.querySelector('.like-buttons__counter_third-like-button');
+let firstButton = document.querySelector('.js-like-buttons__border');
+let secondButton = document.querySelector('.js-like-buttons__border_second-like-button');
+let thirdButton = document.querySelector('.js-like-buttons__border_third-like-button');
+
+let firstCounter = document.querySelector('.js-like-buttons__counter');
+let secondCounter = document.querySelector('.js-like-buttons__counter_second-like-button');
+let thirdCounter = document.querySelector('.js-like-buttons__counter_third-like-button');
+
+let firstHeart = document.querySelector('.js-like-buttons__heart');
+let secondHeart = document.querySelector('.js-like-buttons__heart_second');
+let thirdHeart = document.querySelector('.js-like-buttons__heart_third');
+
 let countLikesOfFirstButton = 0;
 let countLikesOfSecondButton = 0;
 let countLikesOfThirdButton = 0;
 
-firstButton.addEventListener('click', e=>{
-    _svg1.classList.add('on');
+firstButton.addEventListener('click', e => {
+    firstHeart.classList.remove('like-buttons__heart');
+    firstHeart.classList.add('like-buttons__heart_clicked');
     firstButton.classList.add('on');
     firstCounter.classList.add('on');
     countLikesOfFirstButton++;
     firstCounter.innerHTML = countLikesOfFirstButton;
 }, false);
 
-secondButton.addEventListener('click', e=>{
-    _svg2.classList.add('on');
+secondButton.addEventListener('click', e => {
+    secondHeart.classList.remove('like-buttons__heart_second');
+    secondHeart.classList.add('like-buttons__heart_second_clicked');
     secondButton.classList.add('on');
     secondCounter.classList.add('on');
     countLikesOfSecondButton++;
     secondCounter.innerHTML = countLikesOfSecondButton;
 }, false);
 
-thirdButton.addEventListener('click', e=>{
-    _svg3.classList.add('on');
+thirdButton.addEventListener('click', e => {
+    thirdHeart.classList.remove('like-buttons__heart_third');
+    thirdHeart.classList.add('like-buttons__heart_third_clicked');
     thirdButton.classList.add('on');
     thirdCounter.classList.add('on');
     countLikesOfThirdButton++;
