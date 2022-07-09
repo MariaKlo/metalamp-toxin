6ea53341308ed6bec689576588316211125ac567
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-07-09 19:08:33 +0300

    Refactor buttons for form-elements

--- src/components/buttons/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/components/buttons/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,39 +1,53 @@
-let firstList = document.querySelector('.filter__checkbox-list');
-let secondList = document.querySelector('.filter__checkbox-list_second');
+class expandableCheckboxList {
 
-let showFirstItem = document.querySelector('.filter__checkbox_first');
-let showSecondItem = document.querySelector('.filter__checkbox_second');
-let showThirdItem = document.querySelector('.filter__checkbox_third');
-let showFourthItem = document.querySelector('.filter__checkbox_fourth');
-let showFifthItem = document.querySelector('.filter__checkbox_fifth');
-let showSixthItem = document.querySelector('.filter__checkbox_sixth');
+  constructor(list, arrow) {
+    this.list = list;
+    this.arrow = arrow;
+    this.init();
+  }
 
-let showSeventhItem = document.querySelector('.filter__checkbox_seventh');
-let showEighthItem = document.querySelector('.filter__checkbox_eighth');
-let showNinthItem = document.querySelector('.filter__checkbox_ninth');
-let showTenthItem = document.querySelector('.filter__checkbox_tenth');
-let showEleventhItem = document.querySelector('.filter__checkbox_eleventh');
-let showTwelfthItem = document.querySelector('.filter__checkbox_twelfth');
+  init() {
+    this.openList();
+  }
 
-let firstArrow = document.querySelector('.filter__checkbox-list-arrow_first');
-let secondArrow = document.querySelector('.filter__checkbox-list-arrow_second');
+  openList() {
+    this.list.addEventListener('click', e => {
+      const itemArrOfFirstList = ['.filter__checkbox_first', '.filter__checkbox_second', 
+      '.filter__checkbox_third', '.filter__checkbox_fourth', '.filter__checkbox_fifth', 
+      '.filter__checkbox_sixth']; 
+      const itemArrOfSecondList = ['.filter__checkbox_seventh', 
+        '.filter__checkbox_eighth', '.filter__checkbox_ninth', '.filter__checkbox_tenth', 
+        '.filter__checkbox_eleventh', '.filter__checkbox_twelfth'];
 
-firstList.addEventListener('click', e=>{
-    showFirstItem.classList.toggle('show');
-    showSecondItem.classList.toggle('show');
-    showThirdItem.classList.toggle('show');
-    showFourthItem.classList.toggle('show');
-    showFifthItem.classList.toggle('show');
-    showSixthItem.classList.toggle('show');
-    firstArrow.classList.toggle('rotate');
-}, false);
+      let firstList = document.querySelector('.filter__checkbox-list');
+      let secondList = document.querySelector('.filter__checkbox-list_second');
 
-secondList.addEventListener('click', e=>{
-    showSeventhItem.classList.toggle('show');
-    showEighthItem.classList.toggle('show');
-    showNinthItem.classList.toggle('show');
-    showTenthItem.classList.toggle('show');
-    showEleventhItem.classList.toggle('show');
-    showTwelfthItem.classList.toggle('show');
-    secondArrow.classList.toggle('rotate');
-}, false);
+      let firstArrow = document.querySelector('.filter__checkbox-list-arrow_first');
+      let secondArrow = document.querySelector('.filter__checkbox-list-arrow_second');
+
+      if (this.list === firstList && this.arrow === firstArrow) {
+        for (let i = 0; i < itemArrOfFirstList.length; i++) {
+          let itemDom = document.querySelector(itemArrOfFirstList[i]);
+          itemDom.classList.toggle('show');
+        }
+      }
+      else if (this.list === secondList && this.arrow === secondArrow) {
+        for (let i = 0; i < itemArrOfSecondList.length; i++) {
+         let itemDomSecond = document.querySelector(itemArrOfSecondList[i]);
+          itemDomSecond.classList.toggle('show');
+        }
+      }
+      this.arrow.classList.toggle('rotate');
+    }, false);
+  }
+}
+
+const list = ['.filter__checkbox-list', '.filter__checkbox-list_second'];
+const arrow = ['.filter__checkbox-list-arrow_first', '.filter__checkbox-list-arrow_second'];
+
+for (let i = 0; i < list.length; i++) {
+  let listDom = document.querySelector(list[i]);
+  let arrowDom = document.querySelector(arrow[i]);
+
+  let expandableList = new expandableCheckboxList(listDom, arrowDom);
+}
--- src/components/buttons/like-button/like-button.js
+++ src/components/buttons/like-button/like-button.js
@@ -1,42 +1,42 @@
-let firstButton = document.querySelector('.js-like-buttons__border');
-let secondButton = document.querySelector('.js-like-buttons__border_second-like-button');
-let thirdButton = document.querySelector('.js-like-buttons__border_third-like-button');
+class likeButton {
 
-let firstCounter = document.querySelector('.js-like-buttons__counter');
-let secondCounter = document.querySelector('.js-like-buttons__counter_second-like-button');
-let thirdCounter = document.querySelector('.js-like-buttons__counter_third-like-button');
+  constructor(border, heart, counter) {
+    this.border = border;
+    this.heart = heart;
+    this.counter = counter;
+    this.init();
+  }
 
-let firstHeart = document.querySelector('.js-like-buttons__heart');
-let secondHeart = document.querySelector('.js-like-buttons__heart_second');
-let thirdHeart = document.querySelector('.js-like-buttons__heart_third');
+  init() {
+    this.countOnClick();
+    this.addClassToDom();
+  }
 
-let countLikesOfFirstButton = 0;
-let countLikesOfSecondButton = 0;
-let countLikesOfThirdButton = 0;
+  countOnClick() {
+    this.border.addEventListener('click', e => {
+      let count = 0;
+      count++;
+      this.counter.innerHTML = count;
+    }, false);
+  }
 
-firstButton.addEventListener('click', e => {
-    firstHeart.classList.remove('like-buttons__heart');
-    firstHeart.classList.add('like-buttons__heart_clicked');
-    firstButton.classList.add('on');
-    firstCounter.classList.add('on');
-    countLikesOfFirstButton++;
-    firstCounter.innerHTML = countLikesOfFirstButton;
-}, false);
+  addClassToDom() {
+    this.border.addEventListener('click', e => {
+      this.heart.classList.add('clicked');
+      this.border.classList.add('on');
+      this.counter.classList.add('on');
+    }, false);
+  }
+}
 
-secondButton.addEventListener('click', e => {
-    secondHeart.classList.remove('like-buttons__heart_second');
-    secondHeart.classList.add('like-buttons__heart_second_clicked');
-    secondButton.classList.add('on');
-    secondCounter.classList.add('on');
-    countLikesOfSecondButton++;
-    secondCounter.innerHTML = countLikesOfSecondButton;
-}, false);
+const button = ['.js-like-buttons__border', '.js-like-buttons__border_second-like-button', '.js-like-buttons__border_third-like-button'];
+const counter = ['.js-like-buttons__counter', '.js-like-buttons__counter_second-like-button', '.js-like-buttons__counter_third-like-button'];
+const heart = ['.js-like-buttons__heart', '.js-like-buttons__heart_second', '.js-like-buttons__heart_third'];
 
-thirdButton.addEventListener('click', e => {
-    thirdHeart.classList.remove('like-buttons__heart_third');
-    thirdHeart.classList.add('like-buttons__heart_third_clicked');
-    thirdButton.classList.add('on');
-    thirdCounter.classList.add('on');
-    countLikesOfThirdButton++;
-    thirdCounter.innerHTML = countLikesOfThirdButton;
-}, false);
+for (let i = 0; i < heart.length; i++) {
+   let buttonDom = document.querySelector(button[i]);
+   let counterDom = document.querySelector(counter[i]);
+   let heartDom = document.querySelector(heart[i]);
+
+   let like = new likeButton(buttonDom, heartDom, counterDom);
+}
