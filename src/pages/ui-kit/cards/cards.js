24308ec74871f99d0f95bdc983e7c079108f4237
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-07-20 15:41:09 +0300

    Edit uikit styles, remove errors

--- src/pages/ui-kit/cards/cards.js
+++ src/pages/ui-kit/cards/cards.js
@@ -4,10 +4,6 @@ import './cards.sass';
 
 import '../../../components/plugins/datepicker/datepicker.js';
 import '../../../components/plugins/item-quantity-dropdown/item-quantity-dropdown.js';
-import '../../../components/plugins/pagination/pagination.js';
-import '../../../components/plugins/range-slider/range-slider.js';
 import '../../../components/plugins/swiper/swiper.js';
-import '../../../components/buttons/like-button/like-button.js';
-import '../../../components/buttons/expandable-checkbox-list/expandable-checkbox-list.js';
 
 global.jQuery = global.$ = $;
