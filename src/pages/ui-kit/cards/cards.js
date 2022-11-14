11a74ddf52dc8a578274f17193d6299dd56d4ef5
MariaKlo <56316185+MariaKlo@users.noreply.github.com>
2022-11-14 20:02:20 +0300

    Fixing rate buttons #6

--- src/pages/ui-kit/cards/cards.js
+++ src/pages/ui-kit/cards/cards.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 import './cards.sass';
 
+import '../../../components/buttons/rate-button/rate-button';
+
 import '../../../components/plugins/datepicker/datepicker';
 import '../../../components/plugins/item-quantity-dropdown/item-quantity-dropdown';
 import '../../../components/plugins/swiper/swiper';
